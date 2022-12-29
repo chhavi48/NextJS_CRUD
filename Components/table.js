beac640167d4a8452172761a637113fc918a18fa
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-12-29 18:36:29 +0530

    redux

--- Components/table.js
+++ Components/table.js
@@ -1,7 +1,15 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-import data from '../database/data.json'
-
+import { getUsers } from "../lib/helper";
+import { useQuery } from 'react-query';
+import { useSelector } from "react-redux";
 export default function Table(){
+//  const state=useSelector((state)=>state)
+//      console.log(state)
+    const {isLoading,isError,data,error} = useQuery('users', getUsers)
+//  console.log(data)
+    if(isLoading) return <div>Employee is Loading...</div>;
+    if(isError) return <div>Got Error {error}</div>
+
     return (
         <table className="min-w-full table-auto">
             <thead>
@@ -28,7 +36,7 @@ export default function Table(){
             </thead>
             <tbody className="bg-gray-200">
                     {
-                        data.map((obj, i) => <Tr {...obj} key={i} />)
+                      data.users.map((obj, i) =><Tr {...obj} key={i}/>)
                     }
             </tbody>
         </table>
@@ -39,7 +47,7 @@ function Tr({id, name, avatar, email, salary, date, status }){
     return (
         <tr className="bg-gray-50 text-center">
         <td className="px-16 py-2 flex flex-row items-center">
-            <img src={avatar || '#'} alt="" />
+            <img src={avatar || '#'} alt="" className="h-8 w-8 rounded-full object-cover" />
             <span className="text-center ml-2 font-semibold">{name || "Unknown"}</span>
         </td>
         <td className="px-16 py-2">
@@ -52,7 +60,7 @@ function Tr({id, name, avatar, email, salary, date, status }){
             <span>{date || "Unknown"}</span>
         </td>
         <td className="px-16 py-2">
-            <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">{status || "Unknown"}</span></button>
+            <button className="cursor"><span className={`${status == "Active" ? 'bg-green-500' : 'bg-rose-500'} text-white px-5 py-1 rounded-full`}>{status || "Unknown"}</span></button>
         </td>
         <td className="px-16 py-2 flex justify-around gap-5">
             <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
