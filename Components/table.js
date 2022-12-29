fc4ddb990ad5b426012ac7e547d6f807f5b07184
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-12-29 10:44:23 +0530

    video-8

--- Components/table.js
+++ Components/table.js
@@ -1,4 +1,6 @@
-import {BiEdit,BiTrashAlt} from 'react-icons/bi'
+import { BiEdit, BiTrashAlt } from "react-icons/bi";
+import data from '../database/data.json'
+
 export default function Table(){
     return (
         <table className="min-w-full table-auto">
@@ -20,47 +22,42 @@ export default function Table(){
                         <span className="text-gray-200">Status</span>
                     </th>
                     <th className="px-16 py-2">
-                        <span className="text-gray-200">Action</span>
+                        <span className="text-gray-200">Actions</span>
                     </th>
-                    
                 </tr>
-
             </thead>
-            <tbody className="bg-gray-200" >
-          <tr className="bg-gray-50 text-center">
-            <td className="px-16 py-2 flex flex-row items-center">
-                <img src="#" alt=""/>
-                <span className="text-center ml-2 font-semibold">Chhavii </span>
-            </td>
-            <td className="px-16 py-2">
-                <span>chhavi48@gmail.com</span>
-            </td>
-            <td className="px-16 py-2">
-                <span>$240000</span>
-            </td>
-            <td className="px-16 py-2">
-                <span>7-04-2001</span>
-            </td>
-            <td className="px-16 py-2">
-                <button className="cursor">
-                  <span className="bg-green-500 text-white px-5 py-1 rounded-3" >    Active</span>
-                </button>
-            </td>
-            <td className="px-16 py-2 flex justify-around gap-5">
-            <button className="cursor">
-                <BiEdit size={25} color={"rgba(34,197,94)"}></BiEdit>
-                </button>
-                <button className="cursor">
-                <BiTrashAlt size={25} color={"rgba(244,63,94)"}></BiTrashAlt>
-                </button>
-            </td>
-
-        
-    
-        
-          </tr>
+            <tbody className="bg-gray-200">
+                    {
+                        data.map((obj, i) => <Tr {...obj} key={i} />)
+                    }
             </tbody>
-            
-            </table>
+        </table>
+    )
+}
+
+function Tr({id, name, avatar, email, salary, date, status }){
+    return (
+        <tr className="bg-gray-50 text-center">
+        <td className="px-16 py-2 flex flex-row items-center">
+            <img src={avatar || '#'} alt="" />
+            <span className="text-center ml-2 font-semibold">{name || "Unknown"}</span>
+        </td>
+        <td className="px-16 py-2">
+            <span>{email || "Unknown"}</span>
+        </td>
+        <td className="px-16 py-2">
+            <span>{salary || "Unknown"}</span>
+        </td>
+        <td className="px-16 py-2">
+            <span>{date || "Unknown"}</span>
+        </td>
+        <td className="px-16 py-2">
+            <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">{status || "Unknown"}</span></button>
+        </td>
+        <td className="px-16 py-2 flex justify-around gap-5">
+            <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
+            <button className="cursor"><BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt></button>
+        </td>
+    </tr>
     )
 }
