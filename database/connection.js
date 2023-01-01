3574873a2e12bda9bae73080fb7b274f79e9acf8
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2023-01-01 16:54:45 +0530

    env done

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
-const MONGO_URL='mongodb+srv://chhavi:chhavi123@cluster0.m3aecg1.mongodb.net/?retryWrites=true&w=majority'
+// const MONGO_URL='mongodb+srv://chhavi:chhavi123@cluster0.m3aecg1.mongodb.net/?retryWrites=true&w=majority'
 import mongoose from 'mongoose';
 
 const connectMongo = async () => {
     try{
 
-        const { connection }  = await mongoose.connect(MONGO_URL)
+        const { connection }  = await mongoose.connect(process.env.MONGO_URL)
 
         if(connection.readyState == 1){
             console.log("Database Connected")
